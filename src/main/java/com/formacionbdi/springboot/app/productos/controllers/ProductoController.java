6aea37a7c91232d2ccd54c195a7ee0f44712737b
Jose Tejada <jtejadavilca@gmail.com>
2019-09-03 23:48:51 -0500

    Se completa crud básico de producto

--- src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
+++ src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
@@ -6,9 +6,11 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,4 +49,22 @@ public class ProductoController {
 	public Producto crear(@RequestBody Producto producto) {
 		return productoService.save(producto);
 	}
+	
+	@PutMapping("/editar/{id}")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Producto editar(@RequestBody Producto producto, @PathVariable Long id) {
+		Producto productoBD = productoService.findById(id);
+		productoBD.setNombre(producto.getNombre());
+		productoBD.setPrecio(producto.getPrecio());
+
+		return productoService.save(productoBD);
+	}
+	
+	@DeleteMapping("/eliminar/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void eliminar(@PathVariable Long id) {
+		productoService.deleteById(id);
+	}
+
+
 }
